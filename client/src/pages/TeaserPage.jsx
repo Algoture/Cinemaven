e3fb14659857673b9517e1ae23f632aec6ffba98
samyack15 <djsamyack@gmail.com>
2024-05-25 13:36:28 +0530

    addedteaser ppage

--- client/src/pages/TeaserPage.jsx
+++ client/src/pages/TeaserPage.jsx
@@ -10,28 +10,45 @@ const TeaserPage = () => {
     <>
       <Nav />
       <div className="teaserpage">
-        <div className="teaserdetails">
-          <h1 className="teaserh1">{title}</h1>
-          <h2 className="teaserh1">Release Date : {releaseDate}</h2>
-          <h3 className="teaserh1">Trailer Length : {trailerLength}</h3>
+        
+      <div className="MoviesPage">
+        <div
+          className="gradient"
+          // style={{ "--poster-url": `url(${movie.bgimage})` }}
+          
+        >
+          <img src="https://images.unsplash.com/photo-1595769816263-9b910be24d5f?q=80&w=2958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </div>
-        <div className="teaserCards">
-          <div className="card-trailer">
-            <div className="content-card">
-              <img src={image} alt={title} className="teaserimage" />
+
+        <div className="teaser-details">
+          <h1 className="treaserh1">{title}</h1>
+          <h2 className="teaserh1">Release Date:{releaseDate}</h2>
+          <h3 className="teaserh1">Trailer length:{trailerLength}</h3>
+        </div>
+        <div className="components" >
+          <div className="movie-card card-trailer"style={{width: "20vw"}} >
+            <div className="content-card" style={{width: "20vw"}}>
+            <a href={videoLink} target="_blank">
+            <img src={image} alt={title}  />
               <span className="shadow"></span>
-              <div className="content">
-                <h1 style={{ color: "black", fontSize: "1rem" }}>Trailer</h1>
-                <b>{trailerLength}</b>
+              <div className="content" >
+                <h1>Trailer</h1>
+                {/* <b>{movie.trailerLength}</b> */}
               </div>
+            </a>
+             
             </div>
-            <div className="watch-card">
+            <div className="watch-card" onClick={()=>{
+              window.open(videoLink,"_blank");
+            }}>
               <a href={videoLink} target="_blank">
-                <button>watch now</button>
+                <button href={videoLink}>watch now</button>
+                {console.log(videoLink)}
               </a>
             </div>
           </div>
-          {/* <div className="card-movie" >
+
+{/* <div className="card-movie" >
             <div className="content-card">
               <span className="shadow"></span>
               <div className="content">
@@ -42,8 +59,12 @@ const TeaserPage = () => {
               <button>watch now</button>
             </div>
           </div> */}
-        </div>
+      
       </div>
+      
+          
+        </div>
+    </div>
     </>
   );
 };
