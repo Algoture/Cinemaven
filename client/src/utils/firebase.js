a4fd52acdbba3b78bdc695bf61875ab451da2a23
SheshNalwar <nalwarshesh@gmail.com>
2024-08-16 22:35:56 +0530

    environment variables added in client

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -2,13 +2,13 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_lad1eZ5OaPR0EbRNseAy8z1VIn4tCH8",
-  authDomain: "cinemaven-27d04.firebaseapp.com",
-  projectId: "cinemaven-27d04",
-  storageBucket: "cinemaven-27d04.appspot.com",
-  messagingSenderId: "855871118917",
-  appId: "1:855871118917:web:ee6ef9fedf365a4e11cd2c",
-  measurementId: "G-14C31H7CC6",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
