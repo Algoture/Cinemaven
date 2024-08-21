7221e813571c265209ff6b38f02a7264aee8220a
Umesh Nagare <umeshnagare27@gmail.com>
2024-08-21 21:25:29 +0530

    cors changes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 import router from "./routes/moviesroutes.js";
 import connectDb from "./db/connect.js";
 import express from "express";
+import cors from "cors";
 import { config as configDotenv } from "dotenv";
 configDotenv();
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome To Cinemaven API");
 });
 
 app.use("/api/movies", router);
 const start = async () => {
   await connectDb(process.env.MONGO_URI);
-  app.listen(port);
+  app.listen(port, () => {
+    console.log(`Server is running`);
+  });
 };
 start();
 export { app };
